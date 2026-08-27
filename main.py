d88e3f2e667425a70469089ffc772637ab1a5578
jonatas567 <53832366+jonatas567@users.noreply.github.com>
2026-08-26 21:40:49 -0300

    conexão bd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import psycopg2
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,3 +9,25 @@ def login():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+def get_connection():
+    
+
+    print ( "Entrou na função")
+    try:
+        return psycopg2.connect(
+            host="db.ytfbetitkfnxgvxcfoew.supabase.co",
+            database= "postgres",
+            user= "postgres",
+            password= "Univesp@201",
+            port= 5432
+             )
+        print("conectado ao banco de dados")
+        
+    except Exception as erro:
+        print("Erro na conexão.")
+        print(repr(erro))
+        return None
+
+
+conn = get_connection()
